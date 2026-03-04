005f813f057bd4f107c0a02f5a500c076dc00bee
haine88 <pathamawanhanford@gmail.com>
2026-03-04 09:27:34 -0800

    clean and comment code

--- dogAgeCalculator/dogAgeCalculator.js
+++ dogAgeCalculator/dogAgeCalculator.js
@@ -9,6 +9,7 @@ node dogAgeCalculator.js
 
 */
 
+// Variables for userinput - pet name and human age 
 let name = process.argv[2];
 let humanAge = Number(process.argv[3]);
 
@@ -17,14 +18,17 @@ let humanAge = Number(process.argv[3]);
 // Year 2 = +9 dog years (total: 24)
 // Every year after = +5 dog years
 
+
 let dogAge;
 
+// set condition for human years to dog years
 if (humanAge === 1) {
     dogAge = 15;
 } else if (humanAge === 2) {
     dogAge = 24;
 } else {
-    dogAge = 24 + (humanAge - 2) * 5;
+    dogAge = 24 + (humanAge - 2) * 5; // each year after 2 adds 5 dog years
 }
 
-console.log("Your dog, " + name + ", is " + humanAge + " years old, but that's " + dogAge + " years old in dog years!");
+// print the result 
+console.log(`Your dog  ${name}  is  ${humanAge} years old, but that's  ${dogAge} years old in dog years!`);
